3d1930f8fbdbf16573c2a64d975a614bbadd0d54
yano-kentaro <ynkn0829@gmail.com>
2021-06-27 15:02:58 +0900

    Calendar.vueの作成

---
+++ frontend/vue/src/components/Calendar.vue
@@ -0,0 +1,23 @@
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar
+  },
+  data: function(){
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+      }
+    }
+  }
+}
+</script>
--- frontend/vue/src/views/Schedule.vue
+++ frontend/vue/src/views/Schedule.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="schedule">
-
+    <Calendar />
   </div>
 </template>
 
 <script>
+import Calendar from '@/components/Calendar.vue'
+
 export default {
   name: 'Schedule',
+  components: {
+    Calendar
+  }
 }
 </script>
