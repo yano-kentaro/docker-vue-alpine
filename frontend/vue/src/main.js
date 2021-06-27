2b8c4fee18bc4b6d31bfcc64f7238a26e1078a0e
yano-kentaro <ynkn0829@gmail.com>
2021-06-27 16:03:37 +0900

    main.jsの修正 framework7の有効化

--- frontend/vue/src/main.js
+++ frontend/vue/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Framework7 from 'framework7/lite-bundle';
+import Framework7Vue, { registerComponents } from 'framework7-vue/bundle';
+import 'framework7/framework7-bundle.css';
+Framework7.use(Framework7Vue);
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App).use(store).use(router).mount('#app');
+
+registerComponents(app);
+
+app.mount('#app');
